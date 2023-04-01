8ae8995be32734502aa70be4a6b1563ec3a0357e
mihai9909 <neagoimihai@gmail.com>
2023-04-01 23:19:43 +0300

    CRUD Laboratories

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,8 @@
 class TeachersController < ApplicationController
+  before_action :require_teacher
+
+  private
+  def require_teacher
+    raise ActiveRecord::RecordNotFound unless current_user.is_a?(Teacher)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :defaults => { :format => :json }
   resources :teachers, only: [:index, :create, :update, :destroy], :defaults => { :format => :json }
+  resources :laboratories, only: [:index, :create, :update, :destroy], :defaults => { :format => :json }
 
   resources :students, only: [:index, :create, :update, :destroy], :defaults => { :format => :json } do
     member do
